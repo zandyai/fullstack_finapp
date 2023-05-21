44f3d8bfc3a4abf7aa0476c8d876af86e0f3791b
DyCodeBlock <iam.zandy88@gmail.com>
2023-05-21 11:33:04 +0800

    first commit

---
+++ client/src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "@/App";
+import "@/index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "@/state/api";
+
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
